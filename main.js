8b94152f95c221e0d24a77f533c7340055f33d25
Amr <amrsaleh1001@gmail.com>
2020-04-11 06:22:47 -1000

    and navigation

--- main.js
+++ main.js
@@ -2,12 +2,48 @@ const  carouselSlide =document.querySelector('.carousel-slide');
 const  carouselImage =document.querySelectorAll('.carousel-slide  img');
 var prev= document.getElementById('prevBtn');
 var next= document.getElementById('nextBtn');
+var r1 =document.getElementById('r1');
+var r2 =document.getElementById('r2');
+var r3 =document.getElementById('r3');
+var r4 =document.getElementById('r4');
+var r5 =document.getElementById('r5');
+r1.onclick=function () {
+    console.log('1');
+    carouselSlide.style.transition="transform 0.4s ease-in-out";
+    carouselSlide.style.transform='translateX('+(-size*1)+'px)';
+
+};
+
+r2.onclick=function ()
+{
+    console.log('2');
+    carouselSlide.style.transition="transform 0.4s ease-in-out";
+    carouselSlide.style.transform='translateX('+(-size*2)+'px)';
+};
+r3.onclick=function ()
+{
+    console.log('3');
+    carouselSlide.style.transition="transform 0.4s ease-in-out";
+    carouselSlide.style.transform='translateX('+(-size*3)+'px)';
+};
+r4.onclick=function ()
+{
+    console.log('4');
+    carouselSlide.style.transition="transform 0.4s ease-in-out";
+    carouselSlide.style.transform='translateX('+(-size*4)+'px)';
+};
+r5.onclick=function ()
+{
+    carouselSlide.style.transition="transform 0.4s ease-in-out";
+    carouselSlide.style.transform='translateX('+(-size*5)+'px)';
+};
+
 let i=1;
 const size=carouselImage[0].clientWidth;
 carouselSlide.style.transform='translateX('+(-size*i)+'px)';
 
 
-  //Here put the number of times you want to auto submit
+  // Here put the number of times you want to auto submit
 (function submit(){
     next.click();
     setTimeout(submit, 3000);   //Each second
